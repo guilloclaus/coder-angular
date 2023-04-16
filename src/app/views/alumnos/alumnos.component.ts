2a471d6d8aac88b4a02c0888019d25a605b044f8
guillermo.javier.ojeda.developer <guillermo.javier.ojeda.developer@gmail.com>
2023-04-16 20:45:09 -0300

    Actualizacion de validaciones

--- src/app/views/alumnos/alumnos.component.ts
+++ src/app/views/alumnos/alumnos.component.ts
@@ -9,14 +9,11 @@ import { FormsModule ,ReactiveFormsModule , AbstractControl, FormBuilder, FormCo
 
 export class AlumnosComponent {
 
-
-  emailControl = new FormControl(
-    '',
-    [
-      Validators.required,
-      Validators.email,
-    ]
-  );
+  emailControl = new FormControl('',[Validators.required,Validators.email,]);
+  documentoControl = new FormControl('',[Validators.required,Validators.minLength(7),Validators.maxLength(10)]);
+  ciudadControl = new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]);
+  calleControl = new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(50)]);
+  numeroControl = new FormControl('',[Validators.required,Validators.minLength(1),Validators.maxLength(5)]);
 
   registerForm: FormGroup;
 
@@ -24,27 +21,32 @@ export class AlumnosComponent {
 
     this.registerForm = this.formBuilder.group({
       nombre: ['', [ Validators.required, Validators.minLength(3)] ],
-      apellido: ['', [ Validators.required] ],
+      apellido: ['', [ Validators.required, Validators.minLength(3)] ],
+      documento: this.documentoControl,
+      tipoDocumento: ['', [ Validators.required] ],
       email: this.emailControl,
       localizacion: this.formBuilder.group({
-        ciudad: [''],
-        direccion_1: [''],
+        ciudad: this.ciudadControl,
+        calle:this.calleControl,
+        numero: this.numeroControl,
+        piso: [''],
         provincia: [''],
         cp: [''],
       }),
     });
-
-    console.log(this.registerForm.get('nombre')?.value)
-    console.log(this.emailControl.value);
   }
 
   get nombreControl(): AbstractControl | null {
     return this.registerForm.get('nombre');
   }
-
-
+  get apellidoControl(): AbstractControl | null {
+    return this.registerForm.get('apellido');
+  }
   get nombreControlIsInvalid(): boolean {
-    return !!(this.nombreControl?.invalid && this.nombreControl.touched);
+    return !!((this.nombreControl?.invalid) && this.nombreControl.touched);
+  }
+  get apellidoControlIsInvalid(): boolean {
+    return !!((this.apellidoControl?.invalid) && this.apellidoControl.touched);
   }
 
   onSubmit(): void {
